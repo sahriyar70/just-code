9ff7a46008c82c3c60f13039e95354209c60975d
Al Sahriyar <alsahriyar229@gmail.com>
2026-04-14 11:06:23 +0600

    bill unit

--- moseover/mose.js
+++ moseover/mose.js
@@ -9,5 +9,17 @@ abcd.addEventListener("mouseout",function(){
 })
 
 window.document.addEventListener("keyup", function(e){
-    console.log("Key up:", e.key)
+    // console.log("Key up:", e.key)
 })
+
+
+// let count = 6
+
+// let interverl = setInterval(function(){
+
+//     if(count >= 1 ){
+//         console.log(count--)
+//     } else clearInterval(interverl)
+
+
+// },1000)
